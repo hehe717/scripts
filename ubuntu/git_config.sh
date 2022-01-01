a03fe18ebb0e4aed06b378e4b81d0e0f33e64ac7
justin <nawonjae@naver.com>
2022-01-02 03:51:41 +0900

    add ubuntu scripts

---
+++ ubuntu/git_config.sh
@@ -0,0 +1,2 @@
+git config --global user.email nawonjae@naver.com
+git config --global user.name justin
---
+++ ubuntu/install_common.sh
@@ -0,0 +1,5 @@
+sudo apt-get install -y net-tools
+sudo apt-get install -y vim
+sudo apt-get install -y fcitx-hangul
+sudo apt install -y unzip ubuntu-desktop
+sudo apt-get install -y gufw
